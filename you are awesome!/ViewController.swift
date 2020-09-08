06269d993b00a11453d8c4a937915ad21d6bce18
rdbrumm <rbrumm@zagmail.gonzaga.edu>
2020-09-08 03:38:58 -0700

    added show message button that works

--- you are awesome!/ViewController.swift
+++ you are awesome!/ViewController.swift
@@ -14,8 +14,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("viewDidLoad has run")
+        messageLabel.text = "fabulous, darling"
     }
-
-
+// system events - events triggered by ios like view controller loading
+// user actions - events triggered by the user, like a button being pressed
+    @IBAction func messageButtonPressed(_ sender: UIButton) {
+        print("The message button hath been pressed, King👑")
+        messageLabel.text = "you are awesome!"
+    }
+    
 }
 
