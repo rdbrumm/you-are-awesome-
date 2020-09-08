8e15c1890e54bc7fecaa270efea92b566c06b6f4
rdbrumm <rbrumm@zagmail.gonzaga.edu>
2020-09-08 13:55:53 -0700

    finished 2.6: images - UIImage and the asset catalog

--- you are awesome!/ViewController.swift
+++ you are awesome!/ViewController.swift
@@ -11,22 +11,18 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("viewDidLoad has run")
-        messageLabel.text = "fabulous, darling"
     }
 // system events - events triggered by ios like view controller loading
 // user actions - events triggered by the user, like a button being pressed
     @IBAction func messageButtonPressed(_ sender: UIButton) {
         print("The message button hath been pressed, King👑")
         messageLabel.text = "you are awesome!"
+        imageView.image = UIImage(named: "image0")
     }
     
-    @IBAction func anotherMessageButtonPressed(_ sender: UIButton) {
-        print("The OTHER message button hath been pressed, King👑")
-        messageLabel.text = "you are great!"
-    }
 }
 
